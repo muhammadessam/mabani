c869c1766d0acbffba8a5f462ac456a09f31d1c5
Muhammad Essam <mohammedessam699@gmail.com>
2020-07-26 15:42:15 +0200

    adds some ui

--- public/dist/js/demo.js
+++ public/dist/js/demo.js
@@ -202,7 +202,7 @@
 
 
   function ConvertNumberToPersion() {
-        let persian = { 0: '۰', 1: '۱', 2: '۲', 3: '۳', 4: '۴', 5: '۵', 6: '۶', 7: '۷', 8: '۸', 9: '۹' };
+        let persian = { 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9' };
         function traverse(el) {
             if (el.nodeType == 3) {
                 var list = el.data.match(/[0-9]/g);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,71 @@
 @extends('layouts.layout')
 @section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{\App\Building::all()->count()}}</h3>
 
+                            <p>مباني</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-building-o"></i>
+                        </div>
+                        <a href="{{route('buildings.index')}}" class="small-box-footer">عرض<i class="fa fa-arrow-circle-left"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{\App\Building::all()->count()}}</h3>
+
+                            <p>وحدة</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-home"></i>
+                        </div>
+                        <a href="{{route('buildings.index')}}" class="small-box-footer">عرض<i class="fa fa-arrow-circle-left"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{\App\Building::all()->count()}}</h3>
+
+                            <p>طابق</p>
+                        </div>
+                        <div class="icon">
+                            <i><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-layers-fill"
+                                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M7.765 1.559a.5.5 0 0 1 .47 0l7.5 4a.5.5 0 0 1 0 .882l-7.5 4a.5.5 0 0 1-.47 0l-7.5-4a.5.5 0 0 1 0-.882l7.5-4z" />
+                                    <path fill-rule="evenodd"
+                                          d="M2.125 8.567l-1.86.992a.5.5 0 0 0 0 .882l7.5 4a.5.5 0 0 0 .47 0l7.5-4a.5.5 0 0 0 0-.882l-1.86-.992-5.17 2.756a1.5 1.5 0 0 1-1.41 0l.418-.785-.419.785-5.169-2.756z" />
+                                </svg></i>
+                        </div>
+                        <a href="{{route('buildings.index')}}" class="small-box-footer">عرض<i class="fa fa-arrow-circle-left"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{\App\Building::all()->count()}}</h3>
+
+                            <p>مستأجر</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <a href="{{route('buildings.index')}}" class="small-box-footer">عرض<i class="fa fa-arrow-circle-left"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
